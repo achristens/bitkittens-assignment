6d58202efad1ea3d0578c16acc4a8f89221f3734
Abby Christens <achristens@gmail.com>
2017-11-15 14:59:36 -0500

    Adding loop for kitten array, add kitten image to empty cat-box div

--- cats.js
+++ cats.js
@@ -1,7 +1,28 @@
 document.addEventListener("DOMContentLoaded", function(){
   var summon = document.querySelector('.summon-cats');
+  var catBox = document.querySelectorAll('.cat-box');
 
   summon.addEventListener('click', function(){
-    console.log("Clicked! Give me kittens!");
+    $.ajax({
+      url: 'http://bitkittens.herokuapp.com/cats.json',
+      method: 'GET',
+      dataType: 'JSON'
+    }).done(function(responseData){
+      var array = responseData.cats
+      array.forEach(function(kitty){
+        // Creates a new <img> tag
+        var newImg = document.createElement('img');
+        // Sets the src attribute of the <img> to the cat's photo
+        newImg.src  = kitty['photo']
+        // Sets the alt attribute of the <img> to "Photo of (insert cat name here)"
+        newImg.alt = "Photo of " + kitty.name
+        // Inserts that <img> into one of the empty .cat-box divs
+        catBox.forEach(function(box){
+          if (box.innerHTML === ""){
+            box.append(newImg)
+          }
+        })
+      })
+    })
   });
 });
