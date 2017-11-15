08877f81cdee3f10508ce0531970639cdda40c02
Abby Christens <achristens@gmail.com>
2017-11-15 14:27:58 -0500

    Adding event listener for summon kitties buttton

---
+++ cats.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(){
+  var summon = document.querySelector('.summon-cats');
+
+  summon.addEventListener('click', function(){
+    console.log("Clicked! Give me kittens!");
+  });
+});
